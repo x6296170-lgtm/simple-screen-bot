0830304b21c15903b002b6677b39f6a904acbd4e
x6296170-lgtm <x6296170@gmail.com>
2026-03-20 10:30:50 +0800

    Create App.xaml.cs file

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows;
+
+namespace SimpleScreenBot {
+    public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+        }
+    }
+}
